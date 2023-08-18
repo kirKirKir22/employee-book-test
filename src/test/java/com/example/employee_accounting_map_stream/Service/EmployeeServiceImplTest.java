842171c35fc5bbebfefca01d5ee2505fe66fc487
kirKirKir22 <XcPfZjQi7hRGeN@>
2023-08-18 04:21:08 +0300

    test initialization

---
+++ src/test/java/com/example/employee_accounting_map_stream/Service/EmployeeServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.example.employee_accounting_map_stream.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceImplTest {
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void removeEmployee() {
+    }
+
+    @Test
+    void findEmployee() {
+    }
+
+    @Test
+    void printAll() {
+    }
+}
